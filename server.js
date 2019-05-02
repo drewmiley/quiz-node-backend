cdafb2f9ed487bc1b87ce036ba368963ac4a8b0d
Drew Miley <drew.miley@opencastsoftware.com>
2019-05-02 15:31:47 +0100

    Adds dummy endpoints

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-const Schema = mongoose.Schema;
-const UserSchema = new Schema({
-    name: String
-});
-const User = mongoose.model('User', UserSchema);
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/test');
+// const Schema = mongoose.Schema;
+// const UserSchema = new Schema({
+//     name: String
+// });
+// const User = mongoose.model('User', UserSchema);
 const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
@@ -19,65 +19,91 @@ router.use((req, res, next) => {
 router.get('/', (req, res) => {
     res.json({ message: 'Server running' });
 });
-router.route('/users')
+router.route('/newquiz')
     .post((req, res) => {
-        let user = new User();
-        user.name = req.body.name;
-        user.save(err => {
-            if (err) {
-                res.send(err);
-            }
-            res.json({ message: 'User created!' });
-        });
-    })
+        console.log(req.body.options);
+        res.json({ code: '', quiz: [] });
+    });
+router.route('/quiz/:code')
     .get((req, res) => {
-        User.find((err, users) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(users);
-        });
+        console.log(req.params.code);
+        res.json({ code: '', quiz: [] });
     });
-router.route('/users/:id')
+router.route('/leaderboards/:user*?')
     .get((req, res) => {
-        User.findById(req.params.id, (err, user) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(user);
-        });
-    })
-    .put((req, res) => {
-        User.findById(req.params.id, (err, user) => {
-            if (err) {
-                res.send(err);
-            }
-            user.name = req.body.name;
-            user.save(err => {
-                if (err) {
-                    res.send(err);
-                }
-                res.json({ message: 'User updated!' });
-            });
-
-        });
-    })
-    .delete((req, res) => {
-        User.remove({
-            _id: req.params.id
-        }, (err, user) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json({ message: 'User deleted' });
-        });
+        console.log(req.params.user);
+        res.json([{ code: '', leaderboard: {} }]);
     });
-router.route('/randomusers')
-    .get(async (req, res) => {
-        const endpointResponse = await fetch('https://jsonplaceholder.typicode.com/users');
-        const data = await endpointResponse.json();
-        const users = data.map(user => user.name);
-        res.json(users);
+router.route('/leaderboards/:code')
+    .get((req, res) => {
+        console.log(req.params.code);
+        res.json({ code: '', leaderboard: {} });
     });
+router.route('/answers/:code/:user')
+    .get((req, res) => {
+        console.log(req.params.code);
+        console.log(req.params.user);
+        res.json({ code: '', score: 0, user: '' });
+    });
+// router.route('/users')
+//     .post((req, res) => {
+//         let user = new User();
+//         user.name = req.body.name;
+//         user.save(err => {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             res.json({ message: 'User created!' });
+//         });
+//     })
+//     .get((req, res) => {
+//         User.find((err, users) => {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             res.json(users);
+//         });
+//     });
+// router.route('/users/:id')
+//     .get((req, res) => {
+//         User.findById(req.params.id, (err, user) => {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             res.json(user);
+//         });
+//     })
+//     .put((req, res) => {
+//         User.findById(req.params.id, (err, user) => {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             user.name = req.body.name;
+//             user.save(err => {
+//                 if (err) {
+//                     res.send(err);
+//                 }
+//                 res.json({ message: 'User updated!' });
+//             });
+//
+//         });
+//     })
+//     .delete((req, res) => {
+//         User.remove({
+//             _id: req.params.id
+//         }, (err, user) => {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             res.json({ message: 'User deleted' });
+//         });
+//     });
+// router.route('/randomusers')
+//     .get(async (req, res) => {
+//         const endpointResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await endpointResponse.json();
+//         const users = data.map(user => user.name);
+//         res.json(users);
+//     });
 app.use('/api', router);
 app.listen(8080);
