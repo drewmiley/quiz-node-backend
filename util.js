679a1c81954175718deed5fa728950b22b0b3af0
Drew Miley <drew.miley@opencastsoftware.com>
2019-05-03 12:02:57 +0100

    Generates code

--- util.js
+++ util.js
@@ -1,4 +1,11 @@
-const generateQuizCode = () => 1;
+const generateQuizCode = () => {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getYear() + 1900;
+    const random = Math.floor(Math.random() * 10000);
+    return `${ day < 10 ? '0' + day : day }${ month < 10 ? '0' + month : month }${ year }${ ('0000' + random).slice(-4) }`;
+};
 
 module.exports.transformResponseToQuizSchema = response => {
     const code = generateQuizCode();
